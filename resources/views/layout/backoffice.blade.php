656f53f4e68043b910e545cb9b204c28bec5c7b9
mateo0911 <mateo88g.m@gmail.com>
2024-12-13 20:17:46 -0500

    Pruebas PR

--- resources/views/layout/backoffice.blade.php
+++ resources/views/layout/backoffice.blade.php
@@ -27,6 +27,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{asset('/')}}">Productos</a>
                 </li>
+                
                 <li class="nav-item">
                     <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Otras opciones</a>
                 </li>
